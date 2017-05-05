a2f38eed2ced1d9229676324240694fffe47ed9a
Tim Stenzel <tim.stenzel@uni-ulm.de>
2017-05-05 17:26:56 +0200

    Anfang Parkdetailansicht

--- app/src/main/java/de/uulm/dbis/coaster2go/activities/ParkDetailViewActivity.java
+++ app/src/main/java/de/uulm/dbis/coaster2go/activities/ParkDetailViewActivity.java
@@ -5,11 +5,11 @@ import android.os.Bundle;
 
 import de.uulm.dbis.coaster2go.R;
 
-public class TimsTestActivity extends AppCompatActivity {
+public class ParkDetailViewActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tims_test);
+        setContentView(R.layout.activity_park_detail_view);
     }
 }
