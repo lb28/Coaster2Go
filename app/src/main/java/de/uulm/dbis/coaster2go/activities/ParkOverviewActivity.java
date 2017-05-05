3f20ffbe4d1a85a1820cbd8b3035680979f7dc00
Luis Nicolas Beaucamp <luis.beaucamp@uni-ulm.de>
2017-05-05 20:00:21 +0200

    added tabs in ParkOverviewActivity

--- app/src/main/java/de/uulm/dbis/coaster2go/activities/ParkOverviewActivity.java
+++ app/src/main/java/de/uulm/dbis/coaster2go/activities/ParkOverviewActivity.java
@@ -1,18 +1,35 @@
 package de.uulm.dbis.coaster2go.activities;
 
+import android.app.ActionBar;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.ViewPager;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 import de.uulm.dbis.coaster2go.R;
 
 public class ParkOverviewActivity extends BaseActivity {
 
+    private static final String TAG = "ParkOverviewActivity";
+    private SectionsPagerAdapter mSectionsPagerAdapter;
 
+    /**
+     * The {@link ViewPager} that will host the section contents.
+     */
+    private ViewPager mViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +39,20 @@ public class ParkOverviewActivity extends BaseActivity {
         Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(myToolbar);
 
+        // Create the adapter that will return a fragment for each of the two
+        // sections (all vs. favorites) of the activity.
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+
+        // Set up the ViewPager with the sections adapter.
+        mViewPager = (ViewPager) findViewById(R.id.viewPagerParkTabs);
+        Log.i(TAG, "onCreate: viewPager: " + mViewPager);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+
+        // Hook the TabLayout up to the viewPager
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabLayoutPark);
+        tabLayout.setupWithViewPager(mViewPager);
+
+        // Set up the floatingActionButton
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab_add_park);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,7 +61,6 @@ public class ParkOverviewActivity extends BaseActivity {
                         .setAction("Action", null).show();
             }
         });
-
     }
 
     @Override
@@ -62,6 +92,77 @@ public class ParkOverviewActivity extends BaseActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    /**
+     * A fragment containing a list of parks
+     */
+    public static class PlaceholderFragment extends Fragment {
+        public PlaceholderFragment() {
+        }
+
+        /**
+         * Returns a new instance of this fragment for the given section
+         * number.
+         */
+        public static PlaceholderFragment newInstance(int sectionNumber) {
+            PlaceholderFragment fragment = new PlaceholderFragment();
+            Bundle args = new Bundle();
+            switch (sectionNumber) {
+                case 0:
+                    args.putString("mode", "all");
+                    break;
+                case 1:
+                    args.putString("mode", "favs");
+                    break;
+            }
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_parklist, container, false);
+            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
+            textView.setText("mode: " + getArguments().getString("mode")); // for testing
+            return rootView;
+        }
+    }
+
+    /**
+     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+     * one of the sections/tabs/pages.
+     */
+    private class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            // getItem is called to instantiate the fragment for the given page.
+            // Return a PlaceholderFragment (defined as a static inner class below).
+            return PlaceholderFragment.newInstance(position);
+        }
+
+        @Override
+        public int getCount() {
+            // Show 2 total pages.
+            return 2;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position) {
+                case 0:
+                    return getString(R.string.all_page_title);
+                case 1:
+                    return getString(R.string.favs_page_title);
+                default:
+                    return null;
+            }
+        }
+    }
 
 
 }
